0b75a7e966a3f7004cfd1aeca8e36264c63156ec
Adyson-Lima <adyson_lima@yahoo.com.br>
2023-12-17 17:49:58 -0300

    13° commit - tdd controller: Consegue listar um hardware especifico e retornar status 200? GREEN

--- app/controllers/api/v1/hardwares_controller.rb
+++ app/controllers/api/v1/hardwares_controller.rb
@@ -1,10 +1,14 @@
 class Api::V1::HardwaresController < ApplicationController
 
-  # before_action :set_hardware, only: %i[] # show update destroy
+  before_action :set_hardware, only: %i[show] # show update destroy
 
   def index
     @hardwares = Hardware.all 
     render json: @hardwares
+  end
+
+  def show
+    render json: @hardware
   end
 
 private
